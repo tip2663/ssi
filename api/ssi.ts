a0f9a67a04e4e74327e7cdab32f8253591861ecd
tip2663 <168122748+tip2663@users.noreply.github.com>
2025-05-14 20:01:25 +0200

    post endpoint

--- api/ssi.ts
+++ api/ssi.ts
@@ -1,3 +1,14 @@
-export function GET(request:Request){
+import * as jose from 'jose';
+
+const BOT_JWK = { "crv": "Ed25519", "x": "Qh7WxBLCKdow-GYZMl0nwbHbLdkZecL1YtfDcDajiW0", "kty": "OKP", "alg": "EdDSA", "use": "sig", "kid": "TrUYWGFXBWKMUiZMxQvNHLAlutiH0T7i6rL06IpiBL0" }
+
+export async function POST(request: Request) {
+    const formData = await request.formData()
+    const authcode = formData.get('authorization_code')?.toString()
+    if (authcode) {
+        const {payload} = await jose.jwtVerify(authcode, BOT_JWK)
+        console.log(payload)
+        return new Response('Hello')
+    }
     return new Response('Hello')
 }
