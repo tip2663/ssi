5096043d93fd39abd263add8d46c7136b604ea36
tip2663 <168122748+tip2663@users.noreply.github.com>
2025-05-14 22:23:26 +0200

    uhm

--- api/credential.ts
+++ api/credential.ts
@@ -19,7 +19,7 @@ const issue = (payload: JwtCredentialPayload) => createVerifiableCredentialJwt(p
     did: ISS_DID,
     signer: async (data) => {
         const buffersource = typeof data !== 'string' ? data : new TextEncoder().encode(data);
-        return (await new jose.FlattenedSign(buffersource).setProtectedHeader({}).sign(ISS_PRIV_JWK)).signature
+        return (await new jose.FlattenedSign(buffersource).setUnprotectedHeader({}).sign(ISS_PRIV_JWK)).signature
     },
     alg: 'EdDSA'  // Specify EdDSA as the algorithm
 }, { header: { 'kid': ISS_DID } });
@@ -45,7 +45,7 @@ export async function POST(r: Request) {
                 type: ['VerifiableCredential', 'SubredditMembership'],
                 issuer: {
                     id: ISS_DID,
-                    name: "Reddit VC"
+                    name: "u/devvit-vc"
                 },
                 credentialSubject: {
                     id: walletdid,
