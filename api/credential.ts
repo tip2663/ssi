8ddf2ad4bab458ce84f0b7be0eb129116956ecb2
tip2663 <168122748+tip2663@users.noreply.github.com>
2025-05-14 22:02:26 +0200

    issue

--- api/credential.ts
+++ api/credential.ts
@@ -1,21 +1,22 @@
 import * as jose from 'jose';
+import { createVerifiableCredentialJwt, JwtCredentialPayload } from 'did-jwt-vc'
+import { ISS_DID } from './const.js';
 
 const ISS_PRIV_JWK = JSON.parse(process.env.ISS_PRIV_JWK!)
-const ISS_PUB_JWK = process.env.ISS_PUB_JWK
 
-// const issue = (payload: JwtCredentialPayload) => createVerifiableCredentialJwt(payload, {
-//     did: 'did:web:waltid-qr-demo.vercel.app',
-//     signer: async (data) => {
-//         const key = await importJWK(JSON.parse(process.env.PRIV_JWK!), 'ES256') as CryptoKey;
-//         const sig = await crypto.subtle.sign(
-//             { name: 'ECDSA', hash: 'SHA-256' },
-//             key,
-//             typeof data !== 'string' ? data : new TextEncoder().encode(data)
-//         );
-//         return Buffer.from(sig).toString('base64url');
-//     },
-//     alg: 'ES256'
-// }, { header: { 'kid': 'did:web:waltid-qr-demo.vercel.app#key-1' } })
+const issue = (payload: JwtCredentialPayload) => createVerifiableCredentialJwt(payload, {
+    did: ISS_DID,
+    signer: async (data) => {
+        const buffersource : BufferSource = typeof data !== 'string' ? Buffer.from(data) : new TextEncoder().encode(data)
+        const sig = await crypto.subtle.sign(
+            'EdDSA',
+            ISS_PRIV_JWK,
+            buffersource
+        );
+        return Buffer.from(sig).toString('base64url');
+    },
+    alg: 'EdDSA'
+}, { header: { 'kid': ISS_DID } })
 
 export async function POST(r: Request) {
     console.log('request: ', r)
@@ -31,30 +32,29 @@ export async function POST(r: Request) {
         const didjwk = await jose.importJWK({...JSON.parse(decodeURIComponent(atob((walletdid).split('did:jwk:',2)[1]))),alg})
         await jose.jwtVerify(jsonBodyPayload.proof.jwt, didjwk)
         
-        console.log(didjwk)
 
     //     const subjectId = decodedHeader.kid || '';
     //     console.log('subjectId: ', { subjectId })
 
-    //     const credential = await issue({
-    //         iat: Date.now(),
-    //         sub: subjectId,
-    //         vc: {
-    //             '@context': ['https://www.w3.org/2018/credentials/v1'],
-    //             type: ['VerifiableCredential', 'MyCredential'],
-    //             issuer: {
-    //                 id: "did:web:waltid-qr-demo.vercel.app",
-    //                 name: "qr demo app"
-    //             },
-    //             credentialSubject: {
-    //                 id: subjectId,
-    //                 somenumber: Math.round(Math.random() * 10)
-    //             },
-    //             issuanceDate: new Date().toISOString()
-    //         }
-    //     })
-    //     console.log(credential)
-    //     return new Response(JSON.stringify({ credential }), { headers: { 'content-type': 'application/json' } })
+        const credential = await issue({
+            iat: Date.now(),
+            sub: walletdid,
+            vc: {
+                '@context': ['https://www.w3.org/2018/credentials/v1'],
+                type: ['VerifiableCredential', 'SubredditMembership'],
+                issuer: {
+                    id: ISS_DID,
+                    name: "Reddit VC"
+                },
+                credentialSubject: {
+                    id: walletdid,
+                    somenumber: Math.round(Math.random() * 10)
+                },
+                issuanceDate: new Date().toISOString()
+            }
+        })
+        console.log(credential)
+        return new Response(JSON.stringify({ credential }), { headers: { 'content-type': 'application/json' } })
 
     }
     return new Response('no proof')
