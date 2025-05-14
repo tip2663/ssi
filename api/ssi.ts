779a884987fff271bfcca948bb78abe219eecc86
tip2663 <168122748+tip2663@users.noreply.github.com>
2025-05-14 20:14:25 +0200

    GET

--- api/ssi.ts
+++ api/ssi.ts
@@ -2,7 +2,11 @@ import * as jose from 'jose';
 
 const BOT_JWK = { "crv": "Ed25519", "x": "Qh7WxBLCKdow-GYZMl0nwbHbLdkZecL1YtfDcDajiW0", "kty": "OKP", "alg": "EdDSA", "use": "sig", "kid": "TrUYWGFXBWKMUiZMxQvNHLAlutiH0T7i6rL06IpiBL0" }
 
+export async function GET(request:Request){
+    return 'Hello'
+}
 export async function POST(request: Request) {
+    console.log(request)
     const formData = await request.formData()
     const authcode = formData.get('authorization_code')?.toString()
     if (authcode) {
