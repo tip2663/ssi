a2277070560ed1f97511d45f2b655ba9db4caf29
tip2663 <168122748+tip2663@users.noreply.github.com>
2025-05-14 22:43:18 +0200

    cred

--- api/credential.ts
+++ api/credential.ts
@@ -1,6 +1,7 @@
 import * as jose from 'jose';
 import { JwtCredentialPayload } from 'did-jwt-vc'
 import { ISS_DID, ISS_PRIV_JWK } from './const.js';
+import * as uuid from 'uuid'
 
 // const issue = (payload: JwtCredentialPayload) => createVerifiableCredentialJwt(payload, {
 //     did: ISS_DID,
@@ -15,7 +16,7 @@ import { ISS_DID, ISS_PRIV_JWK } from './const.js';
 //     },
 //     alg: 'EdDSA'
 // }, { header: { 'kid': ISS_DID } })
-const issue = async (payload: JwtCredentialPayload) => new jose.SignJWT({...payload,iss:ISS_DID}).setProtectedHeader({alg:'EdDSA',kid:ISS_DID}).sign(ISS_PRIV_JWK)
+const issue = async (payload: JwtCredentialPayload) => new jose.SignJWT({...payload,iss:ISS_DID}).setProtectedHeader({alg:'EdDSA',kid:ISS_DID,typ:'JWT'}).sign(ISS_PRIV_JWK)
 //  createVerifiableCredentialJwt(payload, {
 //     did: ISS_DID,
 //     signer: async (data) => {
@@ -38,8 +39,10 @@ export async function POST(r: Request) {
         const walletdid: string = decodedHeader.kid!
         const didjwk = await jose.importJWK({ ...JSON.parse(decodeURIComponent(atob((walletdid).split('did:jwk:', 2)[1]))), alg })
         await jose.jwtVerify(jsonBodyPayload.proof.jwt, didjwk)
+        const now = Date.now()
         const credential = await issue({
-            iat: Date.now(),
+            iat: now,
+            nbf: now,
             sub: walletdid,
             vc: {
                 '@context': ['https://www.w3.org/2018/credentials/v1'],
@@ -53,7 +56,8 @@ export async function POST(r: Request) {
                     somenumber: Math.round(Math.random() * 10)
                 },
                 issuanceDate: new Date().toISOString()
-            }
+            },
+            jti:`urn:uuid:${uuid.v4()}`
         })
         console.log(credential)
         return new Response(JSON.stringify({ credential }), { headers: { 'cache-control': 'no-store', 'content-type': 'application/json' } })
