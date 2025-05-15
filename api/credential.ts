a46e91895d99d98c26c621fa5690bfb35f80bc1e
tip2663 <168122748+tip2663@users.noreply.github.com>
2025-05-15 12:48:13 +0200

    expire

--- api/credential.ts
+++ api/credential.ts
@@ -2,31 +2,8 @@ import * as jose from 'jose';
 import { JwtCredentialPayload } from 'did-jwt-vc'
 import { ISS_DID, ISS_PRIV_JWK, ISS_PUB_JWK } from './const.js';
 import * as uuid from 'uuid'
-import { create } from 'qrcode';
 
-// const issue = (payload: JwtCredentialPayload) => createVerifiableCredentialJwt(payload, {
-//     did: ISS_DID,
-//     signer: async (data) => {
-//         const buffersource: BufferSource = typeof data !== 'string' ? Buffer.from(data) : new TextEncoder().encode(data)
-//         const sig = await crypto.subtle.sign(
-//             'EdDSA',
-//             ISS_PRIV_JWK,
-//             buffersource
-//         );
-//         return Buffer.from(sig).toString('base64url');
-//     },
-//     alg: 'EdDSA'
-// }, { header: { 'kid': ISS_DID } })
 const issue = async (payload: JwtCredentialPayload) => new jose.SignJWT({...payload,iss:ISS_DID}).setProtectedHeader({alg:'EdDSA',kid:ISS_DID,typ:'JWT'}).sign(ISS_PRIV_JWK)
-//  createVerifiableCredentialJwt(payload, {
-//     did: ISS_DID,
-//     signer: async (data) => {
-//         const buffersource = typeof data !== 'string' ? data : new TextEncoder().encode(data);
-//         return (await (await new jose.GeneralSign(buffersource).addSignature(ISS_PRIV_JWK)).sign()).signatures[0].signature
-//     },
-//     alg: 'EdDSA'  // Specify EdDSA as the algorithm
-// }, { header: { 'kid': ISS_DID } });
-
 export async function POST(r: Request) {
     const jsonBodyPayload = await r.json()
 
@@ -41,11 +18,14 @@ export async function POST(r: Request) {
         const walletdid: string = decodedHeader.kid!
         const didjwk = await jose.importJWK({ ...JSON.parse(decodeURIComponent(atob((walletdid).split('did:jwk:', 2)[1].replace(/#.+$/,'')))), alg })
         await jose.jwtVerify(jsonBodyPayload.proof.jwt, didjwk)
-        const now = Math.floor(Date.now() / 1000) 
+        const now = Math.floor(Date.now() / 1000)
+        const oneYearInSeconds = 31536000;
+
         const credential = await issue({
             iat: now,
             nbf: now,
             sub: walletdid,
+            exp: now + oneYearInSeconds,
             vc: {
                 '@context': ['https://www.w3.org/2018/credentials/v1'],
                 type: ['VerifiableCredential', 'SubredditMembership'],
